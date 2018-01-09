fdc454c1805eb69d7ee33cd0da312436e736fa39
WHTCrawford <william.h.crawford@icloud.com>
2018-01-09 10:52:56 +0000

    data and scripts

---
+++ GALR_new.py
@@ -0,0 +1,167 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+import time
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+# Structure: loop through a big sample by taking minibatches, do this for a number of epochs . Do all of this for
+# however many trials,randomising the learning rate parameters each time
+number_of_trails = 20
+number_of_epochs = 250
+mini_batch_size = 10
+sample_size = 4000
+learn_steps = (number_of_epochs*sample_size) / mini_batch_size
+hidden_layer_size_d = 6
+hidden_layer_size_g = 5
+
+# define actual distribution
+real_mean = 6
+real_sd = 1
+
+
+# discriminator and generator NNs
+def discriminator(input, parameters):
+    pre_0 = tf.to_float(input)
+    activ_1 = tf.add(tf.matmul(pre_0, parameters[0]), parameters[1])
+    pre_1 = tf.tanh(activ_1)
+    activ_2 = tf.add(tf.matmul(pre_1, parameters[2]), parameters[3])
+    pre_2 = tf.tanh(activ_2)
+    activ_3 = tf.add(tf.matmul(pre_2, parameters[4]), parameters[5])
+    output = tf.sigmoid(activ_3)
+    return output
+
+
+def generator(input, parameters):
+    pre_0 = tf.to_float(input)
+    activ_1 = tf.add(tf.matmul(pre_0, parameters[0]), parameters[1])
+    pre_1 = tf.tanh(activ_1)
+    output = tf.add(tf.matmul(pre_1, parameters[2]), parameters[3])
+    return output
+
+
+# Create weights and biases variables
+weight_d_1 = tf.Variable(tf.random_uniform([1, hidden_layer_size_d], minval=0, maxval=1, dtype=tf.float32))
+bias_d_1 = tf.Variable(tf.random_uniform([hidden_layer_size_d], minval=0, maxval=1, dtype=tf.float32))
+weight_d_2 = tf.Variable(tf.random_uniform([hidden_layer_size_d, hidden_layer_size_d], minval=0, maxval=1, dtype=tf.float32))
+bias_d_2 = tf.Variable(tf.random_uniform([hidden_layer_size_d], minval=0, maxval=1, dtype=tf.float32))
+weight_d_3 = tf.Variable(tf.random_uniform([hidden_layer_size_d, 1], minval=0, maxval=1, dtype=tf.float32))
+bias_d_3 = tf.Variable(tf.random_uniform([1], minval=0, maxval=1, dtype=tf.float32))
+
+weight_g_1 = tf.Variable(tf.random_uniform([1, hidden_layer_size_g], minval=0, maxval=1, dtype=tf.float32))
+bias_g_1 = tf.Variable(tf.random_uniform([hidden_layer_size_g], minval=0, maxval=1, dtype=tf.float32))
+weight_g_2 = tf.Variable(tf.random_uniform([hidden_layer_size_g, 1], minval=0, maxval=1, dtype=tf.float32))
+bias_g_2 = tf.Variable(tf.random_uniform([1], minval=0, maxval=1, dtype=tf.float32))
+
+
+d_parameters = [weight_d_1,bias_d_1, weight_d_2, bias_d_2,weight_d_3, bias_d_3]
+g_parameters = [weight_g_1,bias_g_1, weight_g_2, bias_g_2]
+
+
+# losses
+x = tf.placeholder(tf.float32, shape=(None, 1))
+z = tf.placeholder(tf.float32, shape=(None, 1))
+D1 = discriminator(x,d_parameters)
+D2 = discriminator(generator(z, g_parameters), d_parameters)
+loss_d = tf.reduce_mean(-tf.log(D1) - tf.log(1 - D2))
+loss_g = tf.reduce_mean(-tf.log(D2))
+
+# Game Adaptive Learning Rate
+phi_g = tf.placeholder(tf.float32)
+phi_d = tf.placeholder(tf.float32)
+gamma = tf.placeholder(tf.float32)
+adjuster = (1/2 * tf.log(phi_d/(2*phi_g +phi_d))) / tf.log(0.25)
+
+V = tf.minimum(tf.reduce_mean(tf.log(D1)+tf.log(1-D2)),0)
+V1 = tf.where(tf.is_nan(V), tf.zeros_like(V), V)
+
+learning_rate_d = gamma-phi_d*tf.tanh(adjuster*V1)
+learning_rate_g = gamma + phi_g*(1 + tf.tanh(adjuster*V1))
+
+# Train step
+
+# train_g = tf.train.AdamOptimizer(learning_rate_d).minimize(loss_g, var_list=g_parameters)
+# train_d = tf.train.AdamOptimizer(learning_rate_d).minimize(loss_d, var_list=d_parameters)
+
+train_g = tf.train.GradientDescentOptimizer(learning_rate_g).minimize(loss_g, var_list=g_parameters)
+train_d = tf.train.GradientDescentOptimizer(learning_rate_d).minimize(loss_d, var_list=d_parameters)
+
+
+# train_g = tf.train.MomentumOptimizer(learning_rate_g,0.6).minimize(loss_g, var_list=g_parameters)
+# train_d = tf.train.MomentumOptimizer(learning_rate_d,0.6).minimize(loss_d, var_list=d_parameters)
+
+
+
+f = open('recently_completed_trial.txt','r')
+start_line = int(f.read())+1
+f.close()
+
+simuls = range(start_line,start_line+number_of_trails)
+
+start_time = time.time()
+
+for it in simuls:
+    # sample data
+    generator_input = np.random.uniform(0, 1, (sample_size, 1))
+    real_dist = np.random.normal(real_mean, real_sd, (sample_size, 1))
+    # sample parameters
+    gamma_vec = np.random.uniform(0.01,0.02,3)  # 0.00001,0.01,3
+    phi_vec = np.random.uniform(0.00001, 0.02, 3) #0.00001, 0.02, 3, phi should be bigger as it is then made smaller by tanh
+    phi_vec[0] = 0.0000001
+
+    res_matrix = np.zeros((len(gamma_vec) * len(phi_vec), sample_size))
+    gamma_out_vec, phi_out_vec = np.zeros((len(gamma_vec) * len(phi_vec))), np.zeros((len(gamma_vec) * len(phi_vec)))
+
+    row =0
+    for i, p in enumerate(phi_vec):
+        for j, k in enumerate(gamma_vec):
+            print 'Trial: {}/{}'.format(it,simuls[len(simuls)-1])
+            print 'Step: {}/{}'.format(i*len(phi_vec)+j+1, len(phi_vec)*len(gamma_vec))
+            print 'Phi: {0}'.format(p)
+            print 'Gamma: {0}'.format(k)
+
+            with tf.Session() as sess:
+                tf.global_variables_initializer().run()
+                for step in range(1, learn_steps):
+                    start_index = ((step - 1) * mini_batch_size) % sample_size
+                    end_index = (step * mini_batch_size) % sample_size
+
+                    x_minibatch = real_dist[start_index:end_index,:]
+                    z_minibatch = generator_input[start_index:end_index,:]
+
+                    sess.run(train_d, feed_dict={x: x_minibatch, z: z_minibatch, phi_g: p,phi_d:p, gamma:k})
+                    sess.run(train_g, feed_dict={x: x_minibatch, z: z_minibatch, phi_g: p,phi_d:p,gamma:k})
+
+                generated = sess.run(generator(generator_input,g_parameters))
+                res_matrix[row] = generated.reshape(sample_size)
+                gamma_out_vec[row] = k
+                phi_out_vec[row] = p
+                row = row+1
+
+                # sns.distplot(generated, hist=False, rug=False)
+                # sns.distplot(real_dist, hist=False, rug=False)
+                # plt.show()
+
+
+
+    res_dataframe = pd.DataFrame(data=res_matrix.astype(float))
+    pd.DataFrame.to_csv(res_dataframe,'data/results{0}.csv'.format(it), sep=',', header=False, float_format='%.6f', index=False)
+
+    gamma_dataframe = pd.DataFrame(data=gamma_out_vec.astype(float))
+    pd.DataFrame.to_csv(gamma_dataframe,'data/gamma{0}.csv'.format(it), sep=',', header=False, float_format='%.8f', index=False)
+
+    phi_dataframe = pd.DataFrame(data=phi_out_vec.astype(float))
+    pd.DataFrame.to_csv(phi_dataframe,'data/phi{0}.csv'.format(it), sep=',', header=False, float_format='%.6f', index=False)
+
+    f = open('recently_completed_trial.txt', 'w')
+    f.write(str(it))
+    f.close()
+
+print 'Total time taken: {0} seconds'.format(time.time()- start_time)
+
+
+
+os.system('afplay /System/Library/Sounds/Sosumi.aiff')
---
+++ analysis.R
@@ -0,0 +1,338 @@
+##################################################
+##################################################
+#
+#   
+#
+##################################################
+##################################################
+rm(list = ls(all=T))
+library(reshape2)
+library(plyr)
+library(ggplot2)
+library(scatterplot3d)
+library(plotly)
+library(lattice)
+source('/Users/Billy/PycharmProjects/GALR/mulitplot.R')
+
+############## WHICH PLOTS #######################
+##################################################
+phi_against_gamma = TRUE
+plot_histograms = F
+interactive_plot = FALSE
+scatter_3D = FALSE
+loess_plot = T
+phi_as_factor = F
+gamma_as_factor = F
+
+
+
+##################################################
+##################################################
+
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
+
+cols = gg_color_hue(2)
+
+collected_data = data.frame()
+
+most_recent_trial = as.numeric(suppressWarnings(
+  read.table('/Users/Billy/PycharmProjects/GALR/GAN-game-adaptive-learning-rates/recently_completed_trial.txt')))
+
+for(trial in 1:most_recent_trial){
+  results = read.csv(paste(
+    paste('/Users/Billy/PycharmProjects/GALR/GAN-game-adaptive-learning-rates/data/results',trial,sep=''),
+    '.csv',sep = ''))
+  
+  gamma = read.csv(paste(
+    paste('/Users/Billy/PycharmProjects/GALR/GAN-game-adaptive-learning-rates/data/gamma',trial,sep=''),
+    '.csv',sep = ''))
+  
+  phi = read.csv(paste(paste('/Users/Billy/PycharmProjects/GALR/GAN-game-adaptive-learning-rates/data/phi',
+                             trial,sep=''),'.csv',sep = ''))
+  
+  trial_data = cbind(cbind(gamma,phi),results)
+  colnames(trial_data) = c('Gamma','Phi',3:1002)
+  collected_data = rbind(collected_data,trial_data)
+  
+}
+
+
+
+sum(collected_data$Phi ==min(collected_data$Phi)) # how many phi zeroes have we collected
+
+gamma_boundaries= c(min(collected_data$Gamma)+
+                      (max(collected_data$Gamma)-min(collected_data$Gamma))/3 ,
+                    min(collected_data$Gamma)+
+                      2*(max(collected_data$Gamma)-min(collected_data$Gamma))/3)
+
+phi_boundaries = c(min(collected_data$Phi),
+                   min(collected_data$Phi)+
+                     (max(collected_data$Phi)-min(collected_data$Phi))/2)
+
+if(phi_against_gamma){
+  cols = gg_color_hue(2)
+  ggplot(collected_data,aes(x = Gamma, y = Phi))+geom_point(col=cols[1])+xlab(bquote(gamma))+ylab(bquote(phi))
+}
+
+
+# create 9 paneled histogram picture
+if(plot_histograms){
+      
+      panel_1 = collected_data[collected_data$Gamma< gamma_boundaries[1] & 
+                                 collected_data$Phi == phi_boundaries[1],3:1002]
+      
+      panel_2 = collected_data[gamma_boundaries[1]< collected_data$Gamma &  
+                                 collected_data$Gamma < gamma_boundaries[2] &
+                                 collected_data$Phi == phi_boundaries[1],3:1002]
+      
+      panel_3 = collected_data[gamma_boundaries[2] < collected_data$Gamma & 
+                                 collected_data$Phi == phi_boundaries[1],3:1002]
+      
+      panel_4 = collected_data[collected_data$Gamma < gamma_boundaries[1] & 
+                                  phi_boundaries[1] < collected_data$Phi &
+                                 collected_data$Phi < phi_boundaries[2],3:1002]
+      
+      panel_5 = collected_data[gamma_boundaries[1]< collected_data$Gamma &  
+                                 collected_data$Gamma < gamma_boundaries[2] &
+                                 phi_boundaries[1] < collected_data$Phi &
+                                 collected_data$Phi < phi_boundaries[2],3:1002]
+      
+      panel_6 = collected_data[gamma_boundaries[2] < collected_data$Gamma & 
+                                 phi_boundaries[1] < collected_data$Phi &
+                                 collected_data$Phi < phi_boundaries[2],3:1002]
+      
+      panel_7 = collected_data[collected_data$Gamma < gamma_boundaries[1] & 
+                                  phi_boundaries[2]< collected_data$Phi,3:1002]
+      
+      panel_8 = collected_data[gamma_boundaries[1]< collected_data$Gamma &  
+                                 collected_data$Gamma < gamma_boundaries[2] &
+                                 phi_boundaries[2]< collected_data$Phi,3:1002]
+      
+      panel_9 = collected_data[gamma_boundaries[2] < collected_data$Gamma &  
+                                 phi_boundaries[2]< collected_data$Phi,3:1002]
+      
+      # take a sample of the rows
+      min_rows = min(c(nrow(panel_1),nrow(panel_2),nrow(panel_3),nrow(panel_4),nrow(panel_5),
+                       nrow(panel_6),nrow(panel_7),nrow(panel_8),nrow(panel_9)))
+    
+      sample_size = min(3,min_rows)
+      
+      panel_1 = data.frame(x = melt(panel_1[sample(1:nrow(panel_1),size = sample_size,replace = F),])[,2])
+      panel_2 = data.frame(x = melt(panel_2[sample(1:nrow(panel_2),size = sample_size,replace = F),])[,2])
+      panel_3 = data.frame(x = melt(panel_3[sample(1:nrow(panel_3),size = sample_size,replace = F),])[,2])
+      panel_4 = data.frame(x = melt(panel_4[sample(1:nrow(panel_4),size = sample_size,replace = F),])[,2])
+      panel_5 = data.frame(x = melt(panel_5[sample(1:nrow(panel_5),size = sample_size,replace = F),])[,2])
+      panel_6 = data.frame(x = melt(panel_6[sample(1:nrow(panel_6),size = sample_size,replace = F),])[,2])
+      panel_7 = data.frame(x = melt(panel_7[sample(1:nrow(panel_7),size = sample_size,replace = F),])[,2])
+      panel_8 = data.frame(x = melt(panel_8[sample(1:nrow(panel_8),size = sample_size,replace = F),])[,2])
+      panel_9 = data.frame(x = melt(panel_9[sample(1:nrow(panel_9),size = sample_size,replace = F),])[,2])
+      
+      
+      # adjust so the scales are the same
+                  
+      pull_max_density =function(panel){
+        min_break = round_any(min(panel$x), diff(range(panel$x))/30, floor)
+        max_break = round_any(max(panel$x), diff(range(panel$x))/30, ceiling)
+        breaks = seq(min_break, max_break, diff(range(panel$x/30)))
+        histo = hist(panel$x, breaks=breaks, plot=F)
+        return(max(histo$density))
+      }
+      
+      max_density = max(c(pull_max_density(panel_1),pull_max_density(panel_2),pull_max_density(panel_3),
+                        pull_max_density(panel_4),pull_max_density(panel_5),pull_max_density(panel_6),
+                        pull_max_density(panel_7),pull_max_density(panel_8),pull_max_density(panel_9)))
+                         
+                      
+      
+      # chart titles
+      
+      title1 = bquote(gamma~'<'~.(round(gamma_boundaries[1],5))~', '~phi~'='~.(round(phi_boundaries[1],5)))
+      
+      title2 = bquote(.(round(gamma_boundaries[1],5))~'<'~gamma~'<'~.(round(gamma_boundaries[2],5))~', '
+                      ~phi~'='~.(round(phi_boundaries[1],5)))
+      
+      title3 = bquote(.(round(gamma_boundaries[2],5))~'<'~gamma~', '~phi~'='~
+                        .(round(phi_boundaries[1],5)))
+      
+      title4 = bquote(gamma~'<'~.(round(gamma_boundaries[1],5))~', '~.(round(phi_boundaries[1],5))~'<'
+                      ~phi~'<'~.(round(phi_boundaries[2],5)))
+      
+      title5 = bquote(.(round(gamma_boundaries[1],5))~'<'~gamma~'<'~.(round(gamma_boundaries[2],5))~', '~
+                        .(round(phi_boundaries[1],5))~'<'~phi~'<'~.(round(phi_boundaries[2],5)))
+      
+      title6 = bquote(.(round(gamma_boundaries[2],5))~'<'~gamma~', '~
+                        .(round(phi_boundaries[1],5))~'<'~phi~'<'~.(round(phi_boundaries[2],5)))
+      
+      title7 = bquote(gamma~'<'~.(round(gamma_boundaries[1],5))~', '~.(round(phi_boundaries[2],5))~'<'~phi)
+      
+      title8 = bquote(.(round(gamma_boundaries[1],5))~'<'~gamma~'<'~.(round(gamma_boundaries[2],5))~
+                        ', '~.(round(phi_boundaries[2],5))~'<'~phi)
+      
+      title9 = bquote(.(round(gamma_boundaries[2],5))~'<'~gamma~
+                        ', '~.(round(phi_boundaries[2],5))~'<'~phi)
+      
+      
+      
+      # ggplot 
+      
+      p1 = ggplot(data.frame(x=panel_1), aes(x)) + geom_histogram(fill = cols[2],aes(y = ..density..))+
+        stat_function(fun = dnorm, args = list(mean = 6, sd = 1), lwd = 1, col = cols[1])+ xlab('')+ ylab('')+
+          labs(title=title1)+ theme(plot.title = element_text(size=8))+ylim(0,max_density)+xlim(4,8)
+      p2 = ggplot(data.frame(x=panel_2), aes(x)) + geom_histogram(fill = cols[2],aes(y = ..density..))+
+        stat_function(fun = dnorm, args = list(mean = 6, sd = 1), lwd = 1, col = cols[1])+ xlab('')+ ylab('')+
+        labs(title=title2)+ theme(plot.title = element_text(size=8))+ylim(0,max_density)
+      p3 = ggplot(data.frame(x=panel_3), aes(x)) + geom_histogram(fill = cols[2],aes(y = ..density..))+
+        stat_function(fun = dnorm, args = list(mean = 6, sd = 1), lwd = 1, 
+                      col = cols[1])+ xlab('')+ ylab('')+labs(title=title3)+ theme(plot.title = element_text(size=8))+
+        ylim(0,max_density)+xlim(4,8)
+      p4 = ggplot(data.frame(x=panel_4), aes(x)) + geom_histogram(fill = cols[2],aes(y = ..density..))+
+        stat_function(fun = dnorm, args = list(mean = 6, sd = 1), lwd = 1, 
+                      col = cols[1])+ xlab('')+ ylab('')+labs(title=title4)+ theme(plot.title = element_text(size=8))+
+        ylim(0,max_density)+xlim(4,8)
+      p5 = ggplot(data.frame(x=panel_5), aes(x)) + geom_histogram(fill = cols[2],aes(y = ..density..))+
+        stat_function(fun = dnorm, args = list(mean = 6, sd = 1), lwd = 1, 
+                      col = cols[1])+ xlab('')+ ylab('')+labs(title=title5)+ theme(plot.title = element_text(size=8))+
+        ylim(0,max_density)+xlim(4,8)
+      p6 = ggplot(data.frame(x=panel_6), aes(x)) + geom_histogram(fill = cols[2],aes(y = ..density..))+
+        stat_function(fun = dnorm, args = list(mean = 6, sd = 1), lwd = 1, 
+                      col = cols[1])+ xlab('')+ ylab('')+labs(title=title6)+ theme(plot.title = element_text(size=8))+
+        ylim(0,max_density)+xlim(4,8)
+      p7 = ggplot(data.frame(x=panel_7), aes(x)) + geom_histogram(fill = cols[2],aes(y = ..density..))+
+        stat_function(fun = dnorm, args = list(mean = 6, sd = 1), lwd = 1, 
+                      col = cols[1])+ xlab('')+ ylab('')+labs(title=title7)+ theme(plot.title = element_text(size=8))+
+        ylim(0,max_density)+xlim(4,8)
+      p8 = ggplot(data.frame(x=panel_8), aes(x)) + geom_histogram(fill = cols[2],aes(y = ..density..))+
+        stat_function(fun = dnorm, args = list(mean = 6, sd = 1), lwd = 1, 
+                      col = cols[1])+ xlab('')+ ylab('')+labs(title=title8)+ theme(plot.title = element_text(size=8))+
+        ylim(0,max_density)+xlim(4,8)
+      p9 = ggplot(data.frame(x=panel_9), aes(x)) + geom_histogram(fill = cols[2],aes(y = ..density..))+
+        stat_function(fun = dnorm, args = list(mean = 6, sd = 1), lwd = 1, 
+                      col = cols[1])+ xlab('')+ ylab('')+labs(title=title9)+ theme(plot.title = element_text(size=8))+
+        ylim(0,max_density)+xlim(4,8)
+      
+      multiplot(p1, p2, p3, p4, p5, p6, p7, p8,p9, cols=3)
+      print(paste('Sample size:',sample_size))
+}
+
+# plot shapiro surface
+
+normal_data = collected_data[,3:1002]
+normal_data = normal_data - 6 # de-mean
+
+
+pull_shapiro=function(row){
+  return(as.numeric(shapiro.test(as.numeric(normal_data[row,]))$statistic))
+}
+
+pull_shapiro2=function(row){
+  return(as.numeric(shapiro.test(as.numeric(normal_data[row,]))$p.value))
+}
+
+
+shapiro_stats = 1-sapply(1:nrow(normal_data),pull_shapiro)
+
+shapiro_data = data.frame(x = collected_data$Gamma, y = collected_data$Phi,z = shapiro_stats)
+
+if(loess_plot){
+    size_of_grid = 100
+    
+    fit_loess = loess(z~x*y,data =shapiro_data, span = 0.4)
+    
+    g_p_grid = expand.grid(list(x = seq(min(shapiro_data$x), max(shapiro_data$x), length.out = size_of_grid), 
+                                     y = seq(min(shapiro_data$y),  max(shapiro_data$y), length.out = size_of_grid)))
+    predicted_shapiro = predict(fit_loess, newdata = g_p_grid)
+    
+    persp(seq(min(shapiro_data$x), max(shapiro_data$x), length.out = size_of_grid),
+          y = seq(min(shapiro_data$y),  max(shapiro_data$y), length.out = size_of_grid),
+          predicted_shapiro, theta = -45,phi = 30,
+          xlab = "Gamma", ylab = "Phi",zlab = 'Shapiro-Wilk Statistic', main = "Shapiro-Wilk Statistic Surface")
+}  
+
+if(scatter_3D){
+    with(shapiro_data, {
+      scatterplot3d(x,   # x axis
+                    y,     # y axis
+                    z,    # z axis
+                    xlab = bquote(gamma),
+                    ylab = bquote(phi),
+                    zlab = bquote('Shapiro')
+      )
+    })
+}
+
+if(interactive_plot){
+  plot_ly(shapiro_data, x = ~x, y = ~y, z = ~z) %>%
+    add_markers() %>%
+    layout(scene = list(xaxis = list(title = 'Gamma'),
+                        yaxis = list(title = 'Phi'),
+                        zaxis = list(title = 'Inverese of Shapiro-Wilk Statistic')),
+           annotations = list(
+             x = 1.13,
+             y = 1.05,
+             xref = 'paper',
+             yref = 'paper',
+             showarrow = FALSE
+           ))
+}
+
+
+shapiro_data$gamma_factors = cut(shapiro_data$x, breaks = c(-Inf, gamma_boundaries, Inf), 
+                               labels = c('low','medium', 'high'), right = TRUE)
+
+
+shapiro_data$phi_factors = cut(shapiro_data$y, breaks = c(-Inf, phi_boundaries, Inf), 
+                                 labels = c('zero','low', 'high'), right = TRUE)
+
+if(phi_as_factor){
+  shapiro_min = min(shapiro_data$z)
+  shapiro_max = max(shapiro_data$z)
+  cols = gg_color_hue(3)
+  mean_1 = data.frame( x = c(-Inf, Inf), y = mean(shapiro_data$z[shapiro_data$phi_factors == 'zero']), 
+                       cutoff = factor(mean(shapiro_data$z[shapiro_data$phi_factors == 'zero'])))
+  mean_2 = data.frame( x = c(-Inf, Inf), y = mean(shapiro_data$z[shapiro_data$phi_factors == 'low']), 
+                       cutoff = factor(mean(shapiro_data$z[shapiro_data$phi_factors == 'low'])))
+  mean_3 = data.frame( x = c(-Inf, Inf), y = mean(shapiro_data$z[shapiro_data$phi_factors == 'high']), 
+                       cutoff = factor(mean(shapiro_data$z[shapiro_data$phi_factors == 'high'])))
+    
+  p1 = ggplot(shapiro_data[shapiro_data$phi_factors == 'zero',], aes(x=x, y=z))+
+    geom_point(col = cols[1])+ xlab(bquote(gamma))+ theme(legend.position="none")+
+    ylab(expression(frac(1,'Shapiro-Wilk Statistic')))+ggtitle(bquote(phi~'='~'0'))+
+    ylim(shapiro_min,shapiro_max)+geom_line(aes( x, y, linetype = cutoff ), mean_1, col = cols[1], lty = 2, lwd = 3)
+  p2 = ggplot(shapiro_data[shapiro_data$phi_factors == 'low',], aes(x=x, y=z))+
+    geom_point(col = cols[2])+ xlab(bquote(gamma))+theme(legend.position="none")+
+    ggtitle(bquote(.(round(phi_boundaries[1],5))~'<'~phi~'<'~.(round(phi_boundaries[2],5))))+
+    ylim(shapiro_min,shapiro_max)+geom_line(aes( x, y, linetype = cutoff ), mean_2, col = cols[2], lty = 2, lwd = 3)
+  p3 = ggplot(shapiro_data[shapiro_data$phi_factors == 'high',], aes(x=x, y=z))+
+    geom_point(col = cols[3])+ xlab(bquote(gamma))+theme(legend.position="none")+
+    ggtitle(bquote(.(round(phi_boundaries[2],5))~'<'~phi))+
+    ylim(shapiro_min,shapiro_max)+geom_line(aes( x, y, linetype = cutoff ), mean_2, col = cols[3], lty = 2, lwd = 3)
+  multiplot(p1, p2, p3, cols=3)
+  
+}
+
+
+if(gamma_as_factor){
+  shapiro_min = min(shapiro_data$z)
+  shapiro_max = max(shapiro_data$z)
+  cols = gg_color_hue(3)
+  p1 = ggplot(shapiro_data[shapiro_data$gamma_factors == 'low',], aes(x=y, y=z))+
+    geom_point(col = cols[1])+geom_smooth(method = lm,se=FALSE,col =cols[1])+ xlab(bquote(phi))+
+    ylab(expression(frac(1,'Shapiro-Wilk Statistic')))+ggtitle(bquote(gamma~'<'~.(round(gamma_boundaries[1],5))))+
+    ylim(shapiro_min,shapiro_max)
+  p2 = ggplot(shapiro_data[shapiro_data$gamma_factors == 'medium',], aes(x=y, y=z))+
+    geom_point(col = cols[2])+geom_smooth(method = lm,se=FALSE,col =cols[2])+ xlab(bquote(phi))+
+    ggtitle(bquote(.(round(gamma_boundaries[1],5))~'<'~gamma~'<'~.(round(gamma_boundaries[2],5))))+
+    ylim(shapiro_min,shapiro_max)
+  p3 = ggplot(shapiro_data[shapiro_data$gamma_factors == 'high',], aes(x=y, y=z))+
+    geom_point(col = cols[2])+geom_smooth(method = lm,se=FALSE,col =cols[2])+ xlab(bquote(phi))+
+    ggtitle(bquote(.(round(gamma_boundaries[2],5))~'<'~gamma))+
+    ylim(shapiro_min,shapiro_max)
+  
+  multiplot(p1, p2, p3, cols=3)
+  
+  
+}
+
